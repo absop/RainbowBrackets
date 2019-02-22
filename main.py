5c14baf5e76b658406f21ca078afe69def9ef03a
absop <2913049342@qq.com>
2019-02-22 16:15:01 +0800

    added color_scheme's caching.

--- main.py
+++ main.py
@@ -16,10 +16,51 @@ class Debuger:
             print(*args)
 
 
+class ColorScheme(object):
+    fgcolors = profile.BRACKETS_COLORS
+    numcolor = len(fgcolors)
+
+    def __init__(self, csname):
+        self.abspath = profile._cache_color_scheme_path(csname, relative=False)
+        self.bgcolor = self.nearest_background(csname)
+        self.rules = []
+        self.nlayer = 0
+        self.add_rules_to(15)
+
+    def add_rules_to(self, nlayer):
+        for i in range(self.nlayer, nlayer):
+            self.rules.append({
+                "name": "Brackets Layer{}".format(i),
+                "scope": profile.BRACKETS_SCOPES.format(i),
+                "foreground": self.fgcolors[i % self.numcolor],
+                "background": self.bgcolor
+            })
+        self.nlayer = nlayer
+
+    def update_colors(self, colors):
+        self.fgcolors = colors
+        self.numcolor = len(colors)
+
+        for i in range(self.nlayer):
+            self.rules[i]["foreground"] = colors[i % self.numcolor]
+
+    def background(self, csname):
+        view = sublime.active_window().active_view()
+        view.settings().set("color_scheme", csname)
+        bgcolor = view.style()["background"]
+        return bgcolor
+
+    def nearest_background(self, csname):
+        bgcolor = self.background(csname)
+        b = int(bgcolor[5:7], 16)
+        b += 1 - 2 * (b == 255)
+        return bgcolor[:-2] + "%02x" % b
+
+
+# 额外提供的 color_scheme, 只需动态提供背景色
+# 以便使括号的背景颜色与颜色方案的背景色一致即可
 class ColorSchemeWriter(object):
-    brackets_colors = profile.BRACKETS_COLORS
-    colors_loop = len(brackets_colors)
-    cached_color_schemes = []
+    cached_color_schemes = {}
     scheme_data = {
        "author": "https://github.com/absop",
        "name": profile.COLOR_SCHEME_NAME,
@@ -28,65 +69,53 @@ class ColorSchemeWriter(object):
        "rules": []
     }
 
-    # 额外提供的 color_scheme, 只需动态提供背景色
-    # 以便使括号的背景颜色与颜色方案的背景色一致即可
-    def __init__(self, color_scheme):
+    def __init__(self, csname):
+        color_scheme = self.cached_color_schemes.get(csname, ColorScheme(csname))
+        if not (self.cached_color_schemes and
+                color_scheme.nlayer >= self.color_scheme.nlayer and
+                os.path.exists(color_scheme.abspath)):
+            if self.cached_color_schemes:
+                color_scheme.add_rules_to(self.color_scheme.nlayer)
+            self.write_color_scheme(color_scheme)
+        self.csname = csname
         self.color_scheme = color_scheme
-        self.abspath = profile._cache_color_scheme_path(color_scheme, relative=False)
-        self.bgcolor = self.nearest_color(self.background(color_scheme))
+        self.cached_color_schemes[csname] = color_scheme
 
-        if not (self.abspath in self.cached_color_schemes and os.path.exists(self.abspath)):
-            self.scheme_data["rules"] = []
-            self.brackets_layer_from_to(0, 15)
-            self.cached_color_schemes.append(self.abspath)
-
-    def update_scheme(self, color_scheme):
-        self.__init__(color_scheme)
-
+    def update_scheme(self, csname):
         def flush_views():
-            Debuger.print("flush_views, color_scheme: ", self.color_scheme)
+            Debuger.print("flush_views with color_scheme: ", csname)
             for window in sublime.windows():
                 for view in window.views():
                     if view.settings().has("rainbow"):
-                        view.settings().set("color_scheme", self.color_scheme)
+                        view.settings().set("color_scheme", csname)
                         Debuger.print("\tfile: ", view.file_name())
 
-        sublime.set_timeout(flush_views, 500)
+        if csname != self.csname:
+            self.__init__(csname)
+            sublime.set_timeout(flush_views, 500)
 
     def update_colors(self, colors):
-        self.brackets_colors = colors
-        self.colors_loop = len(colors)
-        self.scheme_data["rules"] = []
-        self.brackets_layer_from_to(0, self.deepest_layer)
+        self.color_scheme.update_colors(colors)
+        self.cached_color_schemes.pop(self.csname)
+        self.write_color_scheme(self.color_scheme)
 
-    def background(self, color_scheme):
-        view = sublime.active_window().active_view()
-        view.settings().set("color_scheme", color_scheme)
-        bgcolor = view.style()["background"]
-        return bgcolor
+        for color_scheme in self.cached_color_schemes.values():
+            color_scheme.update_colors(colors)
+            self.write_color_scheme(color_scheme)
 
-    # Applies only to #RRGGBB color
-    def nearest_color(self, color):
-        b = int(color[5:7], 16)
-        b += 1 - 2 * (b == 255)
-        return color[:-2] + "%02X" % b
+        self.cached_color_schemes[self.csname] = self.color_scheme
 
-    def brackets_layer_from_to(self, current, deepest):
-        for layer in range(current, deepest):
-            self.scheme_data["rules"].append({
-                "name": "Brackets Layer{}".format(layer),
-                "scope": profile.BRACKETS_SCOPES.format(layer),
-                "foreground": self.brackets_colors[layer % self.colors_loop],
-                "background": self.bgcolor
-            })
-        self.deepest_layer = deepest
+    def add_layer_to(self, nlayer):
+        if nlayer > self.color_scheme.nlayer:
+            Debuger.print("add_rules_from: {} to: {}".format(self.color_scheme.nlayer, nlayer))
+            self.color_scheme.add_rules_to(nlayer)
+            self.write_color_scheme(self.color_scheme)
 
-        with open(self.abspath, "w") as file:
+    def write_color_scheme(self, color_scheme):
+        self.scheme_data["rules"] = color_scheme.rules
+        with open(color_scheme.abspath, "w") as file:
             file.write(json.dumps(self.scheme_data))
-        Debuger.print("write color_scheme: ", self.abspath)
-
-    def brackets_layer_to(self, to):
-        self.brackets_layer_from_to(self.deepest_layer, to)
+        Debuger.print("write file: {}, bg: {}".format(color_scheme.abspath, color_scheme.bgcolor))
 
 
 class BracketsViewListener(object):
@@ -96,6 +125,7 @@ class BracketsViewListener(object):
 
     def add_all_brackets(self, region):
         matched_brackets, unmatched_brackets =  self.searcher.get_brackets_by_layer(region)
+        RainbowBracketsListener.color_scheme_writer.add_layer_to(len(matched_brackets))
         for layer in sorted(matched_brackets):
             regions = [sublime.Region(p, p+1) for p in matched_brackets[layer]]
             key = profile.BRACKETS_SCOPES.format(layer)
@@ -122,32 +152,32 @@ class BracketsViewListener(object):
 class RainbowBracketsListener(sublime_plugin.EventListener):
     view_listeners = {}
 
-    def supported_brackets_of_view(self, view):
-        language = extensions = supported_brackets = None
+    def brackets_of_view(self, view):
+        language = extensions = None
         if view.file_name() and os.path.splitext(view.file_name())[1]:
             extensions = os.path.splitext(view.file_name())[1].lstrip(".")
         if view.settings().get("syntax", None):
             syntax = view.settings().get("syntax", None)
             language = os.path.basename(syntax[:-15]).lower()
-        if language not in self.supported_languages:
-            for lang in self.supported_languages:
-                if extensions in self.supported_languages[lang]["extensions"]:
-                    language = lang
+        if language not in self.languages:
+            for k, v in self.languages.items():
+                if extensions in v["extensions"]:
+                    language = k
                     break
-        if language in self.supported_languages:
-            return self.supported_languages[language].get("brackets", {})
+        if language in self.languages:
+            return self.languages[language].get("brackets", {})
 
     def on_load(self, view):
-        supported_brackets = self.supported_brackets_of_view(view)
-        if supported_brackets:
-            listener = BracketsViewListener(view, supported_brackets)
+        brackets = self.brackets_of_view(view)
+        if brackets:
+            Debuger.print("on_load: file: {}, brackets: {}".format(
+                view.file_name(), brackets))
+
+            listener = BracketsViewListener(view, brackets)
             listener.on_load()
             view.settings().set("color_scheme", self.color_scheme)
             view.settings().set("rainbow", True)
             self.view_listeners[view.id()] = listener
-
-            Debuger.print("on_load: file: {}, brackets: {}".format(
-                view.file_name(), supported_brackets))
 
     def on_modified(self, view):
         if view.id() in self.view_listeners:
@@ -165,14 +195,14 @@ class RainbowBracketsListener(sublime_plugin.EventListener):
             listener.on_selection_modified()
 
     def on_hover(self, view, point, hover_zone):
-       if view.id() in self.view_listeners:
-           listener = self.view_listeners[view.id()]
-           listener.on_hover(point, hover_zone)
+        if view.id() in self.view_listeners:
+            listener = self.view_listeners[view.id()]
+            listener.on_hover(point, hover_zone)
 
     def on_close(self, view):
         if view.id() in self.view_listeners:
             self.view_listeners.pop(view.id())
-            Debuger.print("close_view, file_name: ", view.file_name())
+            Debuger.print("close view: ", view.file_name())
 
     def on_post_save(self, view):
         if not (view.settings().has("rainbow") and view.id() in self.view_listeners):
@@ -197,24 +227,21 @@ class RainbowBracketsListener(sublime_plugin.EventListener):
 
 def load_settings(cls):
     def call_back1():
-        Debuger.print("load_settings: call_back1")
         colors = cls.settings.get("brackets_colors", profile.BRACKETS_COLORS)
         cls.color_scheme_writer.update_colors(colors)
 
     def call_back2():
-        Debuger.print("load_settings: call_back2")
-        supported_languages = cls.settings.get("supported_languages", {})
-        supported_extensions = []
-        for lang in supported_languages:
-            for ext in supported_languages[lang].get("extensions", []):
-                supported_extensions.append(ext if ext[0] == "." else "." + ext)
-            supported_languages[lang.lower()] = supported_languages.pop(lang)
+        languages = cls.settings.get("languages", {})
+        extensions = []
+        for lang in languages:
+            for ext in languages[lang].get("extensions", []):
+                extensions.append(ext if ext[0] == "." else "." + ext)
+            languages[lang.lower()] = languages.pop(lang)
 
-        cls.supported_languages = supported_languages
-        cls.supported_extensions = supported_extensions
+        cls.languages = languages
+        cls.extensions = extensions
 
     def call_back3():
-        Debuger.print("load_settings: call_back3")
         cls.color_scheme = cls.preferences.get("color_scheme")
         cls.color_scheme_writer.update_scheme(cls.color_scheme)
 
